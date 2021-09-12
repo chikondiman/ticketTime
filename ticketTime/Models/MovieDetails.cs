a1a51552077523cd5f8206f4b9e102e0e06feceb
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-12 10:43:05 -0400

    Update MovieDetails.cs

--- ticketTime/Models/MovieDetails.cs
+++ ticketTime/Models/MovieDetails.cs
@@ -7,5 +7,8 @@ namespace ticketTime.Models
 {
     public class MovieDetails
     {
+        public int Id { get; set; }
+        public string Movie_Name { get; set; }
+        public string MyProperty { get; set; }
     }
 }
