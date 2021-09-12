f9ed202758ac949b379c3e03df6be751bfd10e41
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-12 10:47:00 -0400

    Create Cart.cs

---
+++ ticketTime/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ticketTime.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string seatno { get; set; }
+        public string UserId { get; set; }
+        public DateTime date { get; set; }
+    }
+}
