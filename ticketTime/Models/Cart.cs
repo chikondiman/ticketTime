5cd3609d9bf9e21960a2b574494dfc153d4be04c
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-12 10:48:30 -0400

    update

--- ticketTime/Models/Cart.cs
+++ ticketTime/Models/Cart.cs
@@ -11,5 +11,7 @@ namespace ticketTime.Models
         public string seatno { get; set; }
         public string UserId { get; set; }
         public DateTime date { get; set; }
+        public int Amount { get; set; }
+        public int MovieId { get; set; }
     }
 }
